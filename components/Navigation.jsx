1056784b16c7160aab13b0e83e028a63dedc8da2
mce19 <maycolcastroesp19@gmail.com>
2023-05-30 19:59:44 -0600

    lo nuevo de next

---
+++ components/Navigation.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+import styles from '../styles/Navigation.module.css'
+const links = [{
+    label: 'Home',
+    route: '/'
+  },{
+    label: 'About',
+    route: '/about'
+  }]
+  
+
+export default function NavigationPage(){
+
+  return(
+    <header className={styles.header}>
+    <nav>
+    <ul className={styles.navigation}>
+      {links.map(({label, route}) =>(
+        <li key={route}>
+          <Link href={route}>
+            {label}
+            </Link>
+        </li>
+      ))}
+    </ul>
+    </nav>
+  </header>
+  )
+
+}
---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function Aboutpage(){
+
+    return(
+        <>
+        <h1>desde About page</h1>
+        </>
+    )
+}
